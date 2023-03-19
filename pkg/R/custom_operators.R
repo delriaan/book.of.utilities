79a88a5504caf0ede5b0dbf05b7d931affef97cc
Chionesu <sapienti.Dei@outlook.com>
2023-03-19 01:28:14 -0400

    End-of-Day Commit
    1. Began checks in consideration for submission to CRAN
    2. Removed unnecessary functions
    3. Package files now reside in subdirectory 'pkg': this will need to be replicated for all packages

---
+++ pkg/R/custom_operators.R
@@ -0,0 +1,168 @@
+`%bin%` <- function(x, b){
+#' Fixed-Interval Binning Operator
+#'
+#' \code{\%bin\%} creates a vector of values in \code{a} represented as multiples of \code{b}
+#'
+#' @param x (vector) A vector of numeric values
+#' @param b (integer) A bin size to use
+#'
+#' @return The input after subtracting Modulo(b): x - (x %% b)
+#'
+#' @family Custom operators
+#'
+#' @export
+
+	.mod_diff = (x %% b);
+	.mod_diff_ratio = round(.mod_diff/x, 4);
+  .out_data = x - (x %% b);
+	attr(.out_data, "mod_diff_ratio") <- .mod_diff_ratio;
+	.out_data;
+}
+
+#
+test_between <- function(a, z, temporal = FALSE, as.text = FALSE){
+#' "Between" Operator
+#'
+#' \code{test_between} and related operator \code{\%><\%} chunks out each of the inputs using row-wise 'apply' and returns a Boolean result matrix. When arguments \code{a} and \code{z} are vectors or tensors, "between" is interpreted as the existence of the intersection of the domains spanned by the vectors/tensors. When \code{temporal} is \code{TRUE}, an ordinal response is returned depending on the value of \code{as.text}
+#'
+#' \code{as.text FALSE}: the return matrix is comprised of \code{"-1, 0, 1"} denoting \code{"before" "during" "after"}
+#'
+#' \code{as.text TRUE}: the return matrix is comprised of \code{"before" "during" "after"}
+#'
+#' @param a Preferably a data table or data frame, but any object that can be coerced into such
+#' @param z Preferably a data table or data frame, but any object that can be coerced into such
+#' @param temporal (logical) Should relative temporal output (e.g., `before`, `during`, `after`) be returned as integers -1 0 1 rather than logical values?
+#' @param as.text (logical) Should relative temporal output (e.g., `before`, `during`, `after`) be returned as words rather than logical values?
+#'
+#' @return An Boolean object comprised of the results of testing for "between-ness"
+#'
+#' @family Custom operators
+#'
+#' @export
+
+	if (!data.table::is.data.table(a)){ a %<>% data.table::as.data.table() }
+	if (!data.table::is.data.table(z)){ z %<>% data.table::as.data.table() }
+
+	# The case of a single-valued `a` argument is addressed by choosing the `*apply` function based on its length
+	apply.exprs <- list(
+	  stage.apply = list(
+	  		rlang::expr(inner.loop(a))
+	  		, rlang::expr(purrr::as_mapper(~.x[, apply(.x, 1, inner.loop)])(a))
+	  		)
+		, stage.dimname = list(
+				rlang::expr(as.character(a))
+				, rlang::expr(apply(a, 1, stringi::stri_flatten, collapse = ":"))
+				)
+		);
+
+	inner.loop <- function(o){
+	  sub_fn = function(i, j, k) {
+			i %<>% unlist();
+	  	j %<>% unlist();
+	  	k %<>% unlist();
+
+			if (!(j | k)) {
+				# :: Default mode of "between-ness"
+				(min(o, na.rm = TRUE) <= max(i, na.rm = TRUE)) & (max(o, na.rm = TRUE) >= min(i, na.rm = TRUE))
+			} else {
+				# :: Temporal mode to return "before"(-1), "during"(0), "after"(1)
+				t1 = min(o) > max(i, na.rm = TRUE)
+				t2 = (min(o) <= max(i)) & (max(o, na.rm = TRUE) >= min(i, na.rm = TRUE))
+				t3 = c(1, 0, -1)[max(which(t1, t2, TRUE), na.rm = TRUE)]
+
+				# :: Temporal mode to return "before"(-1), "during"(0), "after"(1)
+				if (k) { c("-1" = "before", "0" = "during", "1" = "after")[as.character(t3)] } else { t3 }
+			}
+		}
+		o %<>% unlist();
+	  purrr::as_mapper(~.x[, apply(.x, 1, sub_fn, temporal, as.text)])(z)
+  }
+
+	apply.exprs$stage.apply[[1 + as.integer(length(unlist(a)) > 1)]] %>%
+	  eval() %>%
+	  matrix(
+	  	nrow	= nrow(a), byrow 		= TRUE
+			, dimnames	= list(
+					apply.exprs$stage.dimname[[1 + as.integer(length(unlist(a)) > 1)]] |> eval()
+					, apply(z, 1, stringi::stri_flatten, collapse = ":")
+					)
+		)
+}
+#
+`%><%` <- function(a, z){
+#' \code{\link{test_between}} Operator
+#'
+#' @param a Preferably a data table or data frame, but any object that can be coerced into such
+#' @param z Preferably a data table or data frame, but any object that can be coerced into such
+#'
+#' @return An Boolean object comprised of the results of testing for "between-ness"
+#'
+#' @family Custom operators
+#'
+#' @export
+	test_between(a = a, z = z)
+}
+
+#
+`%tf%` <- function(true = TRUE, false = FALSE){
+#' True-False Operator
+#'
+#' @description
+#' Use \code{\%tf\%} where binary choice vectors can be useful
+#'
+#' @param true (logical) : Scalar or vector value to return as the TRUE  value
+#' @param false (logical) : Scalar or vector value to return as the FALSE value
+#'
+#' @return a \code{\link[data.table]{data.table}} of resultant values options for `true` and `false`
+#'
+#' @family Custom operators
+#'
+#' @export
+
+	if (!rlang::is_empty(dim(true))){ true <- apply(true, 1, as.list) }
+	if (!rlang::is_empty(dim(false))){ false <- apply(false, 1, as.list) }
+
+	# if (length(tr) != length(fls)) { stop("Vectors for TRUE and FALSE must be of the same length: exiting ...") }
+	data.table::as.data.table(list(false = false, true = true, id = seq_along(false)))
+}
+
+#
+`%?%` <- function(cond, result){
+#' Simple IF-THEN-ELSE Operator
+#'
+#' When \code{cond} is \code{TRUE}, \code{result$true} is returned, and the same for \code{cond == FALSE}.  For \code{result}, the easiest way to set the available choices is to use \code{\%::\%}; otherwise, a environment(-like) object with members named 'true' and 'false' must be provided
+#'
+#' @param cond (logical) A \emph{vector} that evaluates to \code{TRUE} or \code{FALSE}
+#' @param result (tensor) Resultant values for TRUE and FALSE conditionals
+#'
+#' @return A \code{\link[data.table]{data.table}} object comprised of values occupying the 'then' and 'else' slots in the 'if-then-else' logical test
+#'
+#' @family Custom operators
+#'
+#' @export
+#'
+
+	data.table::as.data.table(purrr::imap_dfr(cond + 1, ~c(result = rlang::new_box(result[[.x]]), cond_id = .y)))
+}
+
+#
+`%??%` <- function(cond, result){
+#' Correlated IF-THEN-ELSE Comparison
+#'
+#' For each element \code{E} in \code{cond}, when \code{E} is \code{TRUE}, the corresponding index of \code{result$true} is returned: the same for \code{cond == FALSE}.
+#' For \code{result}, the easiest way to set the available choices is to use \code{\link{\%tf\%}}; otherwise, a environment(-like) object with members named \code{true} and \code{false}
+#'
+#' @param cond (logical) A \emph{vector} or \emph{tensor} that evaluates to \code{TRUE} or \code{FALSE}
+#' @param result (vector) Resultant values for TRUE and FALSE conditionals, ideally stored in a dimension-ed object (e.g, \code{\link[base]{data.frame}}, \code{\link[data.table]{data.table}})
+#'
+#' @family Custom operators
+#'
+#' @export
+#'
+
+	if (is.environment(result)){ result <<- data.table::as.data.table(mget(c("true", "false"), envir = result)) }
+	if (!data.table::is.data.table(result)){ result <<- data.table::as.data.table(result) }
+
+	purrr::map2(cond, purrr::array_branch(result, margin = 1), `%?%`) |> purrr::reduce(rbind)
+}
+
---
+++ pkg/R/miscellaneous.R
@@ -0,0 +1,160 @@
+log_note <- function(..., file = "", show = FALSE, append = TRUE){
+#' Create an Entry into a Development Log
+#'
+#' \code{log_note} serves as a wrapper for \code{\link[base]{cat}} and provides an easy way to write to a plain-text file
+#' Even though the original use-case is the real-time capturing of developer notes, it can be used for incremental writing to a file when needed.
+#' @param ... (\code{\link[rlang]{dots_list}}): The string(s) of text to write to the file.  Multiple values are collapsed into a single string separated by a newline. Expressions are allowed and will have the captured output included.
+#' @param file (string|"") The name of the file to (over)write.  If no file is given, the output is sent to the console.
+#' @param show (logical | \code{FALSE}) When \code{TRUE}, print the contents of \code{file} (requires library \code{readtext}).
+#' @param append (logical | \code{TRUE}) When \code{TRUE} new content is appended.
+#'
+#' @return See parameter \code{show}
+#' @export
+
+	tstamp = sprintf("[%s]\n", Sys.time());
+
+	if (...length() > 0){
+		out.txt = paste0(
+			"\n"
+			, tstamp
+			, purrr::map_chr(
+					unname(rlang::exprs(...))
+					, ~ if (!is.character(.x)){ capture.output(eval(.x)) |> paste(collapse = "\n") } else { .x }
+				) %>% paste(collapse = "\n")
+		);
+
+		cat(paste0(out.txt, "\n"), file = file, append = append & !(file == ""));
+	}
+
+	if (show & (!file == "")){ readtext::readtext(file)$text %>% message() }
+}
+#
+vlogical <- function(vector, vpattern, test, simplify_with = NULL, ...){
+#' Vectorized Logical Tests
+#'
+#' \code{vlike} is a vectorized version of \code{\link[data.table]{like}} allowing a pattern vector to be supplied
+#'
+#' @param vector A vector or dimensional object to be processed (e.g., matrix, data.frame, etc.)
+#' @param vpattern A vector of patterns to be matched
+#' @param test (function) The function to use for logical testing: the function should be appropriate for the values of \code{vpattern} and have a named argument of \code{vector}, an additional argument to accept the pattern fed atomically, and '...' (even if not used).
+#' @param simplify_with (function) When provided, the function operates over rows, thereby simplifying the result
+#' @param ... Additional arguments to be sent to the function held by argument \code{test}
+#'
+#' @return A logical matrix, with rows of the same length as \code{vector} and columns the length of \code{vpattern} TRUE for items that match pattern. If vector is dimensional, multiple \code{TRUE} values may be found for each row.
+#'
+#' @export
+
+	if (missing(test)){ test <- data.table::like }
+	if (is.character(test)){ test <- eval(as.symbol(test), envir = globalenv()) }
+	if (!is.function(test)){ message("Argument 'test' is not a function: using default (data.table::like)"); test <- data.table::like; }
+
+	sub_fn = function(v, ...){ sapply(vpattern, purrr::as_mapper(~test(v, .x, ...))) }
+
+	if (class(vector) == "list"){ vector <- unlist(vector); }
+
+	.out = if (any(c("matrix", "array", "data.table", "data.frame", "tibble") %in% class(vector))){ t(apply(vector, 1, sub_fn, ...)) } else { sub_fn(vector, ...)}
+
+	if (!rlang::is_empty(simplify_with)){ apply(.out, 1, simplify_with) } else { provideDimnames(.out, base = list(NULL, c(vpattern))) }
+}
+#
+as.regex <- function(...){
+#' Tag a Regular-Expression String
+#'
+#' \code{as.regex()} adds class "regex" to a strings passed to \code{...}
+#'
+#' @param ... (\code{\link[rlang]{dots_list}}): The string to set "as regex"
+#'
+#' @return The string appended with attribute "regex" set to \code{TRUE}
+#'
+#' @export
+
+	purrr::map(rlang::exprs(...), ~data.table::setattr(eval(.x), name = "regex", value = TRUE));
+}
+#
+is.regex <- function(i){
+#' Test for Class 'regex'
+#'
+#' \code{is.regex} returns \code{TRUE} when the value of \code{i} contains class "regex"
+#'
+#' @param i (string[]) A string or string vector
+#'
+#' @export
+
+	purrr::map(i, ~ifelse(is.null(attr(.x, "regex")), FALSE, attr(.x, "regex"))) |> unlist();
+}
+#
+unregex <- function(i, x){
+#' Convert REGEX Pattern to Object Names
+#'
+#' @param i (string[]) A string or string vector.  Pattern matching is executed if the string is of class regex" (see \code{as.regex}, \code{is.regex})
+#' @param x (object) The names to search OR an object with column names
+#'
+#' @return Matching values in \code{x} based on values of \code{i}
+#'
+#' @export
+	x = if (any(class(x) %in% c("data.table", "data.frame", "tibble"))){ names(x) } else { x }
+
+	reg.needle = i[is.regex(i)] |> unlist();
+
+	no_reg.needle = i[!is.regex(i)] |> unlist();
+
+	reg.match = if (!rlang::is_empty(reg.needle)){ x[grepl(paste(reg.needle, collapse = "|"), x)] }
+
+	no_reg.match = if (!rlang::is_empty(no_reg.needle)){ intersect(x, no_reg.needle) }
+
+	c(if (!any(rlang::is_empty(reg.match))) reg.match, if (!any(rlang::is_empty(no_reg.match))) no_reg.match);
+}
+#
+polyname2orig <- function(poly.names, orig.names, degree, ...){
+#' Polynomial Names to Original
+#'
+#' \code{polyname2orig} renames the column output of the call to \code{\link[stats]{poly}} by replacing each ordinal slot with the original name
+#'
+#' @param poly.names (string[]) The variable names after polynomial conversion
+#' @param orig.names (string[]) The variable names before polynomial conversion
+#' @param degree (int) The degree used in polynomial conversion
+#' @param ... Arguments to send to \code{\link[base]{paste}}, namely \code{sep} (default "_") and \code{collapse} (default ("_x_"))
+#'
+#' @return The polynomial names vector with positions replaced with original name elements
+#'
+#' @export
+
+	.paste.args = list(
+			sep = ifelse("sep" %in% ...names(), list(...)$sep,  "_")
+			, collapse = ifelse("collapse" %in% ...names(), list(...)$collapse, "_x_")
+			);
+
+	.new.names = stringi::stri_split_fixed(poly.names, ".", simplify = TRUE) |>
+    rbind() |>
+		apply(MARGIN = 1, FUN = function(j){ paste(orig.names[j != "0"], j[j != "0"], sep = .paste.args$sep, collapse = .paste.args$collapse) });
+
+  return(.new.names);
+}
+#
+gen.primes <- function(n = 1, domain = 2:n, random = FALSE, distinct = TRUE, chatty = FALSE){
+#' Prime Number Generator
+#'
+#' \code{get.primes} generates \code{n} prime numbers..
+#' @param n (integer) The number of prime numbers to generate
+#' @param domain (integer[]) The range of that prime numbers should fall into given as a length two (2) integer vector
+#' @param random (logical) When \code{TRUE}, the internally-generated vector is subsetted via sampling
+#' @param distinct (logical) When \code{TRUE}, only unique (and sorted) values are returned
+#' @param chatty (logical) When \code{TRUE}, various output is printed to console, mainly for debugging purposes
+#'
+#' @return A vector of prime numbers
+#'
+#' @export
+
+	# Initialize
+	set.seed(Sys.time(), kind = sample(c("Wichmann-Hill", "Marsaglia-Multicarry", "Super-Duper", "Knuth-TAOCP-2002", "L'Ecuyer-CMRG"), 1));
+	x = min(domain, na.rm = TRUE):max(domain, na.rm = TRUE);
+	z = 1:max(domain, na.rm = TRUE);
+
+	logi.vec = rowSums(outer(x, z, `%%`) == 0) == 2;
+
+	output = x[logi.vec];
+	output = if (random){ sample(output, n, replace = TRUE) } else { output[1:n] }
+	output = if (distinct){ unique(output) } else { output }
+	sort(output)
+	# gen.primes(n = 100, domain = c(450, 9571), random = FALSE, distinct = TRUE) %>% { data.table::data.table(x = ., d = c(0, diff(.))) } %T>% print %>% plot
+}
